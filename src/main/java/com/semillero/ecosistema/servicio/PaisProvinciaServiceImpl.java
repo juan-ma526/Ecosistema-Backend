2046a66ef9b85139faef2aab215fbc14a900a80c
Matias <matias.llanos1998@gmail.com>
2024-07-09 17:06:03 -0300

    feat:crear servicio y controlador de pais-provincia

---
+++ src/main/java/com/semillero/ecosistema/servicio/PaisProvinciaServiceImpl.java
@@ -0,0 +1,32 @@
+package com.semillero.ecosistema.servicio;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.semillero.ecosistema.entidad.Pais;
+import com.semillero.ecosistema.entidad.Provincia;
+import com.semillero.ecosistema.repositorio.IPaisRepositorio;
+import com.semillero.ecosistema.repositorio.IProvinciaRepositorio;
+
+@Service
+public class PaisProvinciaServiceImpl {
+
+	@Autowired
+	private IPaisRepositorio paisRepositorio;
+	
+	@Autowired
+	private IProvinciaRepositorio provinciaRepositorio;
+	
+	public List<Pais>mostrarTodo(){
+		return paisRepositorio.findAll();
+	}
+	
+	public List<Provincia> mostrarProvinciasPorPaisId(Long paisId) {
+	    Optional<Pais> opc = paisRepositorio.findById(paisId);
+	    return opc.map(Pais::getProvincia).orElse(Collections.emptyList());
+	}
+}
