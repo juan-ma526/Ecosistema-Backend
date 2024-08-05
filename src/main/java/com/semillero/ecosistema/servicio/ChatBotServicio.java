5b8080ead40680f33fc21eb19be3af5c7f32e403
Matias <matias.llanos1998@gmail.com>
2024-08-05 19:33:51 -0300

    refactor:agrego los metodos

--- src/main/java/com/semillero/ecosistema/servicio/ChatBotServicio.java
+++ src/main/java/com/semillero/ecosistema/servicio/ChatBotServicio.java
@@ -1,11 +1,16 @@
 package com.semillero.ecosistema.servicio;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.semillero.ecosistema.entidad.Pregunta;
 import com.semillero.ecosistema.entidad.Respuesta;
 import com.semillero.ecosistema.repositorio.IPreguntaRepositorio;
 import com.semillero.ecosistema.repositorio.IRespuestaRepositorio;
 
+@Service
 public class ChatBotServicio {
 	
 	@Autowired
@@ -13,9 +18,13 @@ public class ChatBotServicio {
 	@Autowired
 	private IRespuestaRepositorio respuestaRepositorio;
 	
-	public Respuesta mostrarRespuesta() {
-		
-		return null;
+	public Respuesta mostrarRespuesta(Long preguntaId) {
+		Optional<Respuesta>respuesta= respuestaRepositorio.findById(preguntaId);
+		return respuesta.orElse(null);
+	}
+	
+	public Pregunta guardarPregunta(Pregunta pregunta) {
+		return preguntaRepositorio.save(pregunta);	
 	}
 
 }
